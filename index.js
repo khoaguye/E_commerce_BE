0becd716c53da0992676f1754ce1b2de79ee6637
khoaguye <Knguyenn72@gmail.com>
2023-04-20 15:41:49 -0500

    change cors

--- index.js
+++ index.js
@@ -14,9 +14,7 @@ app.use((req, res, next) => {
     next();
 });
 app.use(
-    cors({
-      origin: "http://localhost:3000",
-    })
+   cors()
 );
 app.use(cookieParser()) 
 app.use("/api/product", productRouter)

662150447dab3cf0c3806af522fdbc590d18a7e4
khoaguye <Knguyenn72@gmail.com>
2023-04-21 07:57:30 -0500

    fi

--- index.js
+++ index.js
@@ -13,13 +13,9 @@ app.use((req, res, next) => {
     res.header("Access-Control-Allow-Credentials", true);
     next();
 });
-const cors = require('cors');
-const corsOptions ={
-    origin:'http://localhost:3000', 
-    credentials:true,            //access-control-allow-credentials:true
-    optionSuccessStatus:200
-}
-app.use(cors(corsOptions));
+app.use(
+   cors()
+);
 app.use(cookieParser()) 
 app.use("/api/product", productRouter)
 app.use("/api/user", userInfor)

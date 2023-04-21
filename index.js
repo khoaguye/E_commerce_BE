8c7335df6a2b349564256d00247e36633aad0e90
khoaguye <Knguyenn72@gmail.com>
2023-04-21 07:54:39 -0500

    fix cors

--- index.js
+++ index.js
@@ -13,9 +13,13 @@ app.use((req, res, next) => {
     res.header("Access-Control-Allow-Credentials", true);
     next();
 });
-app.use(
-   cors()
-);
+const cors = require('cors');
+const corsOptions ={
+    origin:'http://localhost:3000', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
 app.use(cookieParser()) 
 app.use("/api/product", productRouter)
 app.use("/api/user", userInfor)

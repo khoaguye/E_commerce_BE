843b98c0c7a23d176abdc4bd4a7dd95357bfd0eb
khoaguye <Knguyenn72@gmail.com>
2023-04-21 22:16:16 -0500

    fixed

--- index.js
+++ index.js
@@ -23,23 +23,23 @@ app.use((req, res, next) => {
 //     optionSuccessStatus:200
 // }
 // app.use(cors(corsOptions));
-// app.use(
-//    cors()
-// ); 
-const allowedOrigins = ['https://e-commerce-fe-ynjo.vercel.app/', 'http://e-commerce-fe-ynjo.vercel.app/'];
+app.use(
+   cors()
+); 
+// const allowedOrigins = ['https://e-commerce-fe-ynjo.vercel.app/', 'http://e-commerce-fe-ynjo.vercel.app/'];
 
-app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  //methods: ["GET","POST","PUT","DELETE"],
-  credentials:true,            //access-control-allow-credentials:true
-   //optionSuccessStatus:200
-}));
+// app.use(cors({
+//   origin: function (origin, callback) {
+//     if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error('Not allowed by CORS'));
+//     }
+//   },
+//   //methods: ["GET","POST","PUT","DELETE"],
+//   credentials:true,            //access-control-allow-credentials:true
+//    //optionSuccessStatus:200
+// }));
 app.use(cookieParser()) 
 app.use("/api/product", productRouter)
 app.use("/api/user", userInfor)

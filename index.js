53ed0108fa74351a78a2979024fa151f05c6773b
khoaguye <Knguyenn72@gmail.com>
2023-04-21 23:43:11 -0500

    asd

--- index.js
+++ index.js
@@ -15,25 +15,11 @@ const app = express()
 //     res.header('Access-Control-Allow-Origin', '*');
 //     next();
 // });
-// const allowedOrigins = ['https://e-commerce-fe-ynjo.vercel.app/', 'http://e-commerce-fe-ynjo.vercel.app/'];
-// const corsOptions ={
-//     origin:allowedOrigins, 
-//     methods: ["GET","POST","PUT","DELETE"],
-//     credentials:true,            //access-control-allow-credentials:true
-//     optionSuccessStatus:200
-// }
-// app.use(cors(corsOptions));
-const allowedOrigins = ['https://e-commerce-fe-ynjo.vercel.app', 'http://e-commerce-fe-ynjo.vercel.app'];
+const allowedOrigins = ['https://e-commerce-fe-ynjo.vercel.app/', 'http://e-commerce-fe-ynjo.vercel.app/'];
 const corsOptions ={
-    origin: function(origin, callback) {
-        if (allowedOrigins.includes(origin)) {
-            callback(null, true);
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
-    },
+    origin:allowedOrigins, 
     methods: ["GET","POST","PUT","DELETE"],
-    credentials: true,            //access-control-allow-credentials:true
+    credentials:true,            //access-control-allow-credentials:true
     optionSuccessStatus:200
 }
 app.use(cors(corsOptions));

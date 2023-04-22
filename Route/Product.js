1795113ffd3aeb490ec218ef1eaf03b492dd54d3
khoaguye <Knguyenn72@gmail.com>
2023-04-21 22:38:12 -0500

    add all cors

--- Route/Product.js
+++ Route/Product.js
@@ -1,6 +1,14 @@
 import express from "express"
 import { allProduct,productContent, productDetail,category, productCategory,searchProduct, addProduct,updateProduct,deleteProduct} from "../Controller/Product.js"
 const router = express.Router();
+const allowedOrigins = ['https://e-commerce-fe-ynjo.vercel.app/', 'http://e-commerce-fe-ynjo.vercel.app/'];
+const corsOptions ={
+    origin:allowedOrigins, 
+    methods: ["GET","POST","PUT","DELETE"],
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+router.use(cors(corsOptions));
 
 //router.get("/productContent", productContent)
 router.get("/allProduct", allProduct)
--- Route/PromotionCode.js
+++ Route/PromotionCode.js
@@ -3,7 +3,14 @@
 import express from "express"
 const router = express.Router();
 import { updatePromo, addPromo, allPromotions, deletePromotion, getOneCode, getCodeByCategory } from "../Controller/PromotionCode.js";
-
+const allowedOrigins = ['https://e-commerce-fe-ynjo.vercel.app/', 'http://e-commerce-fe-ynjo.vercel.app/'];
+const corsOptions ={
+    origin:allowedOrigins, 
+    methods: ["GET","POST","PUT","DELETE"],
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+router.use(cors(corsOptions));
 router.get("/allPromotions", allPromotions)
 router.delete("/deletePromotion", deletePromotion)
 router.post("/addPromo", addPromo)

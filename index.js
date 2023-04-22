b437d71970fe7b9253f4049cbd24004f88effec1
khoaguye <Knguyenn72@gmail.com>
2023-04-21 23:01:07 -0500

    reset

--- index.js
+++ index.js
@@ -47,19 +47,22 @@ import cors from "cors" //npm install cors
 import cookieParser from "cookie-parser"//npm install cookie-parser
 
 const app = express()
-app.use(express.json())
-const allowedOrigins = ['https://e-commerce-fe-ynjo.vercel.app', 'http://e-commerce-fe-ynjo.vercel.app'];
-const corsOptions ={
-    origin: allowedOrigins, 
-    methods: ["GET","POST","PUT","DELETE"],
-    credentials:true,
-    optionSuccessStatus:200
-}
 
-app.use(cors(corsOptions));
+// const allowedOrigins = ['https://e-commerce-fe-ynjo.vercel.app', 'http://e-commerce-fe-ynjo.vercel.app'];
+// const corsOptions ={
+//     origin: allowedOrigins, 
+//     methods: ["GET","POST","PUT","DELETE"],
+//     credentials:true,
+//     optionSuccessStatus:200
+// }
+
+// app.use(cors(corsOptions));
+app.use(
+    cors()
+); 
 
 app.use(cookieParser()) 
-
+app.use(express.json())
 app.use("/api/product", productRouter)
 app.use("/api/user", userInfor)
 app.use("/api/order", orderInfor)

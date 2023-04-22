8eb1907093d3c038ae4037a1a52b97cef27c0d63
khoaguye <Knguyenn72@gmail.com>
2023-04-21 23:27:36 -0500

    checee

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
-// import express from "express";
-// import productRouter from "./Route/Product.js"
-// import userInfor from "./Route/User.js"
-// import orderInfor from "./Route/Order.js"
-// import promoteCode from "./Route/PromotionCode.js"
-// import cors from "cors" //npm install cors
-// import cookieParser from "cookie-parser"//npm install cookie-parser
-// const app = express()
-// //const cors = require('cors');
+import express from "express";
+import productRouter from "./Route/Product.js"
+import userInfor from "./Route/User.js"
+import orderInfor from "./Route/Order.js"
+import promoteCode from "./Route/PromotionCode.js"
+import cors from "cors" //npm install cors
+import cookieParser from "cookie-parser"//npm install cookie-parser
+const app = express()
+//const cors = require('cors');
 
 
-// // app.use((req, res, next) => {
-// //     res.header("Access-Control-Allow-Credentials", true);
-// //     res.header('Access-Control-Allow-Methods', '*');
-// //     res.header('Access-Control-Allow-Origin', '*');
-// //     next();
-// // });
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', '*');
+    res.header('Access-Control-Allow-Origin', '*');
+    next();
+});
 // const allowedOrigins = ['https://e-commerce-fe-ynjo.vercel.app/', 'http://e-commerce-fe-ynjo.vercel.app/'];
 // const corsOptions ={
 //     origin:allowedOrigins, 
@@ -23,48 +23,12 @@
 //     optionSuccessStatus:200
 // }
 // app.use(cors(corsOptions));
-// app.use(express.json())
-// // app.use(
-// //    cors()
-// // ); 
+ app.use(express.json())
+app.use(
+   cors()
+); 
 
-// app.use(cookieParser()) 
-// app.use("/api/product", productRouter)
-// app.use("/api/user", userInfor)
-// app.use("/api/order", orderInfor)
-// app.use("/api/promote", promoteCode)
-
-// app.listen(8800,()=>{
-//     console.log('connected')
-// })
-
-import express from "express";
-import productRouter from "./Route/Product.js"
-import userInfor from "./Route/User.js"
-import orderInfor from "./Route/Order.js"
-import promoteCode from "./Route/PromotionCode.js"
-import cors from "cors" //npm install cors
-import cookieParser from "cookie-parser"//npm install cookie-parser
-
-const app = express()
-
-const allowedOrigins = ['https://e-commerce-fe-ynjo.vercel.app', 'http://e-commerce-fe-ynjo.vercel.app'];
-const corsOptions ={
-    origin: allowedOrigins, 
-    methods: ["GET","POST","PUT","DELETE"],
-    credentials:true,
-    optionSuccessStatus:200
-}
-
-app.use(cors(corsOptions));
-
-// app.use(
-//     cors()
-// ); 
-
-app.use(express.json())
 app.use(cookieParser()) 
-
 app.use("/api/product", productRouter)
 app.use("/api/user", userInfor)
 app.use("/api/order", orderInfor)
@@ -73,3 +37,39 @@ app.use("/api/promote", promoteCode)
 app.listen(8800,()=>{
     console.log('connected')
 })
+
+// import express from "express";
+// import productRouter from "./Route/Product.js"
+// import userInfor from "./Route/User.js"
+// import orderInfor from "./Route/Order.js"
+// import promoteCode from "./Route/PromotionCode.js"
+// import cors from "cors" //npm install cors
+// import cookieParser from "cookie-parser"//npm install cookie-parser
+
+// const app = express()
+
+// const allowedOrigins = ['https://e-commerce-fe-ynjo.vercel.app', 'http://e-commerce-fe-ynjo.vercel.app'];
+// const corsOptions ={
+//     origin: allowedOrigins, 
+//     methods: ["GET","POST","PUT","DELETE"],
+//     credentials:true,
+//     optionSuccessStatus:200
+// }
+
+// app.use(cors(corsOptions));
+
+// // app.use(
+// //     cors()
+// // ); 
+
+// app.use(express.json())
+// app.use(cookieParser()) 
+
+// app.use("/api/product", productRouter)
+// app.use("/api/user", userInfor)
+// app.use("/api/order", orderInfor)
+// app.use("/api/promote", promoteCode)
+
+// app.listen(8800,()=>{
+//     console.log('connected')
+// })

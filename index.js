43f0d7431fb3fc4f867a896b028b31211d34efc3
khoaguye <Knguyenn72@gmail.com>
2023-04-21 23:39:33 -0500

    change cors

--- index.js
+++ index.js
@@ -23,8 +23,20 @@ const app = express()
 //     optionSuccessStatus:200
 // }
 // app.use(cors(corsOptions));
-app.use(cors());
-app.options('*', cors());
+const allowedOrigins = ['https://e-commerce-fe-ynjo.vercel.app', 'http://e-commerce-fe-ynjo.vercel.app'];
+const corsOptions ={
+    origin: function(origin, callback) {
+        if (allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    methods: ["GET","POST","PUT","DELETE"],
+    credentials: true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
  app.use(express.json())
 // app.use(
 //    cors()

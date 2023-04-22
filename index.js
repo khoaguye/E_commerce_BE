62b4ee7845fe9eb4dadd1185ad1bedc52e5f2436
khoaguye <Knguyenn72@gmail.com>
2023-04-21 20:43:06 -0500

    reverse cors

--- index.js
+++ index.js
@@ -14,16 +14,16 @@ app.use((req, res, next) => {
     next();
 });
 
-const corsOptions ={
-    origin:'*', 
-    // methods: ["GET","POST","PUT","DELETE"],
-    credentials:true,            //access-control-allow-credentials:true
-    optionSuccessStatus:200
-}
-app.use(cors(corsOptions));
-// app.use(
-//    cors()
-// );
+// const corsOptions ={
+//     origin:'*', 
+//     // methods: ["GET","POST","PUT","DELETE"],
+//     credentials:true,            //access-control-allow-credentials:true
+//     optionSuccessStatus:200
+// }
+// app.use(cors(corsOptions));
+app.use(
+   cors()
+);
 app.use(cookieParser()) 
 app.use("/api/product", productRouter)
 app.use("/api/user", userInfor)

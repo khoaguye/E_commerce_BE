384dc3e45116a3acb95e335265184fcdce86c145
khoaguye <Knguyenn72@gmail.com>
2023-04-21 23:20:31 -0500

    mess

--- index.js
+++ index.js
@@ -61,6 +61,12 @@ app.use(
     cors()
 ); 
 
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', '*');
+    res.header('Access-Control-Allow-Origin', '*');
+    next();
+});
 app.use(cookieParser()) 
 app.use(express.json())
 app.use("/api/product", productRouter)

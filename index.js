c106d97aeb4671b8653089273438bf146a88148c
khoaguye <Knguyenn72@gmail.com>
2023-04-21 23:24:35 -0500

    abc

--- index.js
+++ index.js
@@ -48,28 +48,23 @@ import cookieParser from "cookie-parser"//npm install cookie-parser
 
 const app = express()
 
-// const allowedOrigins = ['https://e-commerce-fe-ynjo.vercel.app', 'http://e-commerce-fe-ynjo.vercel.app'];
-// const corsOptions ={
-//     origin: allowedOrigins, 
-//     methods: ["GET","POST","PUT","DELETE"],
-//     credentials:true,
-//     optionSuccessStatus:200
-// }
+const allowedOrigins = ['https://e-commerce-fe-ynjo.vercel.app', 'http://e-commerce-fe-ynjo.vercel.app'];
+const corsOptions ={
+    origin: allowedOrigins, 
+    methods: ["GET","POST","PUT","DELETE"],
+    credentials:true,
+    optionSuccessStatus:200
+}
 
-// app.use(cors(corsOptions));
+app.use(cors(corsOptions));
 
+// app.use(
+//     cors()
+// ); 
 
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Credentials", true);
-    res.header('Access-Control-Allow-Methods', '*');
-    res.header('Access-Control-Allow-Origin', '*');
-    next();
-});
-app.use(
-    cors()
-); 
-app.use(cookieParser()) 
 app.use(express.json())
+app.use(cookieParser()) 
+
 app.use("/api/product", productRouter)
 app.use("/api/user", userInfor)
 app.use("/api/order", orderInfor)

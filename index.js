2d5e35ba6c96b09d5ab78ba8ef49f1f7bd8e629f
khoaguye <Knguyenn72@gmail.com>
2023-04-21 23:29:23 -0500

    acc

--- index.js
+++ index.js
@@ -15,18 +15,18 @@ app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     next();
 });
-// const allowedOrigins = ['https://e-commerce-fe-ynjo.vercel.app/', 'http://e-commerce-fe-ynjo.vercel.app/'];
-// const corsOptions ={
-//     origin:allowedOrigins, 
-//     methods: ["GET","POST","PUT","DELETE"],
-//     credentials:true,            //access-control-allow-credentials:true
-//     optionSuccessStatus:200
-// }
-// app.use(cors(corsOptions));
+const allowedOrigins = ['https://e-commerce-fe-ynjo.vercel.app/', 'http://e-commerce-fe-ynjo.vercel.app/'];
+const corsOptions ={
+    origin:allowedOrigins, 
+    methods: ["GET","POST","PUT","DELETE"],
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
  app.use(express.json())
-app.use(
-   cors()
-); 
+// app.use(
+//    cors()
+// ); 
 
 app.use(cookieParser()) 
 app.use("/api/product", productRouter)

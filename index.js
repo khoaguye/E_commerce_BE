9d35c3c52b4c5768facdb0b16fab158b96fdce93
khoaguye <Knguyenn72@gmail.com>
2023-04-21 22:42:25 -0500

    abc

--- index.js
+++ index.js
@@ -1,3 +1,43 @@
+// import express from "express";
+// import productRouter from "./Route/Product.js"
+// import userInfor from "./Route/User.js"
+// import orderInfor from "./Route/Order.js"
+// import promoteCode from "./Route/PromotionCode.js"
+// import cors from "cors" //npm install cors
+// import cookieParser from "cookie-parser"//npm install cookie-parser
+// const app = express()
+// //const cors = require('cors');
+
+
+// // app.use((req, res, next) => {
+// //     res.header("Access-Control-Allow-Credentials", true);
+// //     res.header('Access-Control-Allow-Methods', '*');
+// //     res.header('Access-Control-Allow-Origin', '*');
+// //     next();
+// // });
+// const allowedOrigins = ['https://e-commerce-fe-ynjo.vercel.app/', 'http://e-commerce-fe-ynjo.vercel.app/'];
+// const corsOptions ={
+//     origin:allowedOrigins, 
+//     methods: ["GET","POST","PUT","DELETE"],
+//     credentials:true,            //access-control-allow-credentials:true
+//     optionSuccessStatus:200
+// }
+// app.use(cors(corsOptions));
+// app.use(express.json())
+// // app.use(
+// //    cors()
+// // ); 
+
+// app.use(cookieParser()) 
+// app.use("/api/product", productRouter)
+// app.use("/api/user", userInfor)
+// app.use("/api/order", orderInfor)
+// app.use("/api/promote", promoteCode)
+
+// app.listen(8800,()=>{
+//     console.log('connected')
+// })
+
 import express from "express";
 import productRouter from "./Route/Product.js"
 import userInfor from "./Route/User.js"
@@ -5,43 +45,21 @@ import orderInfor from "./Route/Order.js"
 import promoteCode from "./Route/PromotionCode.js"
 import cors from "cors" //npm install cors
 import cookieParser from "cookie-parser"//npm install cookie-parser
+
 const app = express()
-//const cors = require('cors');
 
-
-// app.use((req, res, next) => {
-//     res.header("Access-Control-Allow-Credentials", true);
-//     res.header('Access-Control-Allow-Methods', '*');
-//     res.header('Access-Control-Allow-Origin', '*');
-//     next();
-// });
-const allowedOrigins = ['https://e-commerce-fe-ynjo.vercel.app/', 'http://e-commerce-fe-ynjo.vercel.app/'];
+const allowedOrigins = ['https://e-commerce-fe-ynjo.vercel.app', 'http://e-commerce-fe-ynjo.vercel.app'];
 const corsOptions ={
-    origin:allowedOrigins, 
+    origin: allowedOrigins, 
     methods: ["GET","POST","PUT","DELETE"],
-    credentials:true,            //access-control-allow-credentials:true
+    credentials:true,
     optionSuccessStatus:200
 }
+
 app.use(cors(corsOptions));
 app.use(express.json())
-// app.use(
-//    cors()
-// ); 
-
-
-// app.use(cors({
-//   origin: function (origin, callback) {
-//     if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-//       callback(null, true);
-//     } else {
-//       callback(new Error('Not allowed by CORS'));
-//     }
-//   },
-//   //methods: ["GET","POST","PUT","DELETE"],
-//   credentials:true,            //access-control-allow-credentials:true
-//    //optionSuccessStatus:200
-// }));
 app.use(cookieParser()) 
+
 app.use("/api/product", productRouter)
 app.use("/api/user", userInfor)
 app.use("/api/order", orderInfor)

59726de5021f50a9fc741d4b74173e4f8e9b84d9
khoaguye <Knguyenn72@gmail.com>
2023-04-21 21:18:55 -0500

    allow method

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ app.use(express.json())
 
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', '*');
+    res.header('Access-Control-Allow-Origin', '*');
     next();
 });
 
